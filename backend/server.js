089b02b01eace2b07eea650bcd0f0de483724ec8
Youssef-Darwish <youssefdarwish95@gmail.com>
2020-11-05 09:48:49 +0200

    Fetch products from database

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,7 @@
 const express = require("express");
-const products = require("./data/products");
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
-
+const productRoutes = require('./routes/productRoutes');
 dotenv.config();
 
 connectDB();
@@ -12,14 +11,7 @@ app.get("/", (req, res) => {
   res.send("API is running");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use('/api/products',productRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`));
