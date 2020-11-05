b8a947bd93f56a8e2c8560f7daf8607045b62cad
Youssef-Darwish <youssefdarwish95@gmail.com>
2020-11-05 14:08:55 +0200

    Custom Error Handling

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const productRoutes = require('./routes/productRoutes');
+const { notFound, errorHandler} = require('./middleware/errorMiddleware');
 dotenv.config();
 
 connectDB();
@@ -12,6 +13,10 @@ app.get("/", (req, res) => {
 });
 
 app.use('/api/products',productRoutes);
+
+// Not found and error handler middlewares
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`));
