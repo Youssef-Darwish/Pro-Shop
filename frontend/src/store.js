c89c25f378176bda3e5aeb587ca24095e3ed13a7
Youssef-Darwish <youssefdarwish95@gmail.com>
2020-11-17 10:34:32 +0200

    Shipping screen and save address

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -26,9 +26,13 @@ const userInfoFromStorage = localStorage.getItem("userInfo")
   ? JSON.parse(localStorage.getItem("userInfo"))
   : null;
 
+const shippingAddressFromStorage = localStorage.getItem("shippingAddress")
+  ? JSON.parse(localStorage.getItem("shippingAddress"))
+  : null;
+
 const initialState = {
-  cart: { cartItems: cartItemsFromStorage },
-  userLogin: { userInfo: userInfoFromStorage }
+  cart: { cartItems: cartItemsFromStorage, shippingAddress: shippingAddressFromStorage },
+  userLogin: { userInfo: userInfoFromStorage },
 };
 
 const middleware = [thunk];
