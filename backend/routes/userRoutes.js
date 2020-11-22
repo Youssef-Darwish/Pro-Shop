cfa7b39b9df7595693c435d9e1b549895629a4a2
Youssef-Darwish <youssefdarwish95@gmail.com>
2020-11-22 10:17:15 +0200

    admin middleware and get users endpoint

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { authUser, registerUser, getUserProfile, updateUserProfile } = require ('../controllers/userController')
-const { protect } = require('../middleware/authMiddleware')
+const { authUser, registerUser, getUserProfile, updateUserProfile, getUsers } = require ('../controllers/userController')
+const { protect, admin } = require('../middleware/authMiddleware')
 
 
 router.post('/login', authUser)
-router.post('/',registerUser)
+router.route('/',registerUser).get(protect, admin, getUsers)
 //protect the route by passing the middleware as first argument
 router.route('/profile').get(protect, getUserProfile)
 router.route('/profile').put(protect, updateUserProfile)
