1d9881140c4351294952a308af35115dda2a4bf1
Youssef-Darwish <youssefdarwish95@gmail.com>
2020-11-24 13:29:08 +0200

    Create and update product endpoints

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -35,5 +35,60 @@ const deleteProductById = asyncHandler(async (req, res) => {
     throw new Error("Product not found");
   }
 });
+// @desc    Create a product
+// @route   POST /api/products
+// @access  Private/Admin
+const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: "Sample Name",
+    price: 0,
+    user: req.user._id,
+    image: "/images/sample.jpg",
+    brand: "sample brand",
+    category: "sample category",
+    countInStock: 0,
+    numReviews: 0,
+    description: "sample description",
+  });
 
-module.exports = { getProducts, getProductById, deleteProductById };
+  const createdProduct = await product.save();
+  res.status(201).json(createdProduct);
+});
+// @desc    Update a product
+// @route   PUT /api/products/:id
+// @access  Private/Admin
+const updateProduct = asyncHandler(async (req, res) => {
+  const {
+    name,
+    price,
+    description,
+    image,
+    brand,
+    category,
+    countInStock,
+  } = req.body;
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    product.name = name;
+    product.price = price;
+    product.description = description;
+    product.category = category;
+    product.image = image;
+    product.brand = brand;
+    product.countInStock = countInStock;
+
+    const updatedProduct = await product.save();
+    res.json(updateProduct);
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
+
+module.exports = {
+  getProducts,
+  getProductById,
+  deleteProductById,
+  createProduct,
+  updateProduct,
+};
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -5,12 +5,15 @@ const {
   getProducts,
   getProductById,
   deleteProductById,
+  createProduct,
+  updateProduct,
 } = require("../controllers/productController");
 
 router.route("/").get(getProducts);
 router
   .route("/:id")
   .get(getProductById)
-  .delete(protect, admin, deleteProductById);
-
+  .delete(protect, admin, deleteProductById)
+  .post(protect, admin, createProduct)
+  .put(protect, admin, updateProduct);
 module.exports = router;
